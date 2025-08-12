b2ff7a18b2229e5faafbf8864fe8a0baf7bd5fbd
Nguyễn Hùng Tuấn Kiệt <tuankiet2962003@gmail.com>
2025-08-12 17:36:00 +0700

    edit GachaCardSeeder

--- backend-laravel/database/seeders/GachaCardSeeder.php
+++ backend-laravel/database/seeders/GachaCardSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\GachaCard;
+use App\Models\Gacha;
+use App\Models\Card;
 
 class GachaCardSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class GachaCardSeeder extends Seeder
      */
     public function run(): void
     {
-        GachaCard::factory()->count(10)->create();
+        foreach(Gacha::all() as $gacha) {
+            $selectedCards = Card::all()->random(rand(3, 5));
+            $rates = $selectedCards->map(fn() => rand(1, 100))->toArray();
+            $totalRate = array_sum($rates);
+            $normalizedRates = array_map(fn($r) => round($r / $totalRate, 4), $rates);
+            foreach($selectedCards as $index => $card) {
+                $gacha->cards()->attach($card->id, [
+                    'rate' => $normalizedRates[$index],
+                    'quantity' => rand(1, 5),
+                    'position' => $index + 1,
+                ]);
+            }
+        }
     }
 }
